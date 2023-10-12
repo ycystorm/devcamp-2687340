5b9b27a300bcb4c62f4c80b42ef3a649a64f1329
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-10-11 17:37:16 -1200

    modificaciones de rutas y request

--- server.js
+++ server.js
@@ -17,6 +17,208 @@
       })
   )
 
+  //ruta de los recursos botcamps
+
+
+  //obtener todos los butcamps
+  app.get('/api/v1/bootcamps', (request, response)=>
+  response
+    .status(200)
+    .json({
+      "success": true,
+      "msg":"aqui van a mostrarse todos los bootcamps"
+    })
+)
+  //obtener  los butcamps con id 
+  app.get('/api/v1/devcamp/bootcamps/:id', (request, response)=>
+  response
+    .status(200)
+    .json({
+      "success": true,
+      "msg":`obteniendo bootcamp con id ${request.params.id}`
+    })
+)
+
+  //crear bootcamp
+  app.post('/api/v1/devcamp/bootcamps', (request, response)=>
+  response
+    .status(200)
+    .json({
+      "success": true,
+      "msg":"aqui se van a crear bootcamps"
+    })
+)
+
+  //actualizar bootcamp
+  app.put('/api/v1/devcamp/bootcamps/actualizar', (request, response)=>
+  response
+    .status(200)
+    .json({
+      "success": true,
+      "msg":"aqui se van a actualizar bootcamps"
+    })
+)
+
+  //eliminar bootcamp
+  app.delete('/api/v1/devcamp/bootcamps/eliminar', (request, response)=>
+  response
+    .status(200)
+    .json({
+      "success": true,
+      "msg":"aqui se van a eliminar bootcamps"
+    })
+)
+
+
+
+
+
+// crud de usuarios 
+
+//crear usuario
+app.post('/api/v1/devcamp/usuario', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se insertaran usuarios"
+  })
+)
+
+//eliminar usuario 
+app.delete('/api/v1/devcamp/usuario/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se eliminaran usuarios con id ${request.params.id}`
+  })
+)
+
+//consultar usuario 
+app.get('/api/v1/devcamp/usuario', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se consultaran todos los usuarios  "
+  })
+)
+
+//consutar usuario por id 
+app.get('/api/v1/devcamp/usuario/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se consultaran todos los usuarios con id  ${request.params.id}  `
+  })
+)
+
+//actualizar usuarios
+app.put('/api/v1/devcamp/usuario/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se actualizaran usuarios con id  ${request.params.id}`
+  })
+)
+//cursos
+//actualizar cursos
+app.put('/api/v1/devcamp/curso/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se actualizaran los cursos con id  ${request.params.id}`
+  })
+)
+//crear cursos
+app.post('/api/v1/devcamp/cursos', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se insertaran cursos"
+  })
+)
+//eliminar cursos
+app.delete('/api/v1/devcamp/cursos/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se eliminaran los cursos con id ${request.params.id}`
+  })
+)
+//consultar cursos
+app.get('/api/v1/devcamp/cursos', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se consultaran todos los cursos "
+  })
+)
+
+//consultar cursos por id 
+app.get('/api/v1/devcamp/cursos/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se consultaran todos los cursos con id  ${request.params.id}`
+  })
+)
+
+//riviews
+
+//consultar reviews
+app.get('/api/v1/devcamp/reviews', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se consultaran  todas las reviews "
+  })
+)
+
+//consultar reviews por id 
+app.get('/api/v1/devcamp/reviews/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se consultaran  todas las reviews con id  ${request.params.id}`
+  })
+)
+
+//crear reviews
+app.post('/api/v1/devcamp/reviews', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":"aqui se crearan las reviews"
+  })
+)
+
+//eliminar reviews
+app.delete('/api/v1/devcamp/reviews/:id', (request, response)=>
+response
+  .status(200)
+  .json({
+    "success": true,
+    "msg":`aqui se eliminaran las reviews con id ${request.params.id}`
+  })
+)
+
+
+
+
+
+
   
   //crear el servidor de aplicacion 
   app.listen(5000 ,
